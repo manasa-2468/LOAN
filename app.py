92bb94f77e4e3a3d4697a7aa4516b9aea7f4fcba
manasa-2468 <manasamanasa89891@gmail.com>
2026-03-31 15:25:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import streamlit as st
 import joblib
-clf = joblib.load('loan.joblib')
-st.title('LOAN APP RCE')
-g = st.number_input('Enter Gender 0:Male 1:Female')
-m = st.number_input('Enter Martial Status 0:No 1:Yes')
-ai = st.number_input('Enter Applicant income in thousands')
-la= st.number_input('Enter loan amount in thousands')
+clf=joblib.load('Loan.joblib')
+st.title('LOAN PREDICTION APP')
+g=st.number_input('Enter Gender 0:Male 1:Female')
+m=st.number_input('Enter Martial Status 0:No 1:Yes')
+ai=st.number_input('Enter Applicant Income in thousands')
+la=st.number_input('Enter Loan amount in thousands')
 if st.button('PREDICT'):
-    prediction = clf.predict([[g,m,ai,la]])
+    prediction=clf.predict([[g,m,ai,la]])
     if prediction=='Y':
         st.text('LOAN IS APPROVED')
     else:
